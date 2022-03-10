6ae745582bbbce38292506008939067e82e3e664
Christine <cenguyen9@gmail.com>
2022-03-10 15:09:31 -0600

    refactored code, practice arrow functions

--- src/book.js
+++ src/book.js
@@ -1,65 +1,89 @@
-function createTitle(title) {
-  // goal of createTitle is to add "The" to the title
-  // create The variable
-  // if no title given, return empty?
+// function createTitle(title) {
+//   // goal of createTitle is to add "The" to the title
+//   // create The variable
+//   // if no title given, return empty?
+//   let addThe = "The";
+//   if (!title) {
+//     return "";
+//   } else {
+//     return `${addThe} ${title}`
+//   }
+// }
+
+let createTitle = title => {
   let addThe = "The";
-  if (!title) {
-    return "";
-  } else {
-    return `${addThe} ${title}`
-  }
+  if (!title) return "";
+  else return `${addThe} ${title}`
 }
 
-function buildMainCharacter(name, age, pronouns) {
-  // goal is to create an object with properties name, age, pronouns
-  // parameters are name, age, pronouns
-  let charObj = {name, age, pronouns}
-  return charObj;
+// function buildMainCharacter(name, age, pronouns) {
+//   // goal is to create an object with properties name, age, pronouns
+//   // parameters are name, age, pronouns
+//   let charObj = {name, age, pronouns}
+//   return charObj;
+// }
+
+let buildMainCharacter = (name, age, pronouns) => {
+  return {name, age, pronouns}
 }
 
-function saveReview(review, reviews) {
-  // goal is to add review to an array
-  // 2 parameters, review to be added and reviews array
-  // it should add new reviews
-  // it should only add unique reviews (no dupes)
-    // check if reviews exists in array - indexOf? includes?
-    // if it does not have it
-  if (!reviews.includes(review)) {
-    reviews.push(review);
-  }
+// function saveReview(review, reviews) {
+//   // goal is to add review to an array
+//   // 2 parameters, review to be added and reviews array
+//   // it should add new reviews
+//   // it should only add unique reviews (no dupes)
+//     // check if reviews exists in array - indexOf? includes?
+//     // if it does not have it
+//   if (!reviews.includes(review)) {
+//     reviews.push(review);
+//   }
+//   return reviews;
+// }
+
+let saveReview = (review, reviews) => {
+  if (!reviews.includes(review)) {reviews.push(review);}
   return reviews;
 }
 
-function calculatePageCount(title) {
-  // goal is to add 20 pages per letter
-    // including spaces? dont forget The
-  // 1 parameter title
-  // call createTitle? 
-  // get length of title and multiply by 20
-  return title.length * 20;
+// function calculatePageCount(title) {
+//   // goal is to add 20 pages per letter
+//     // including spaces? dont forget The
+//   // 1 parameter title
+//   // call createTitle? 
+//   // get length of title and multiply by 20
+//   return title.length * 20;
 
+// }
+
+let calculatePageCount = title => title.length * 20;
+
+// function writeBook(title, character, genre) {
+//   // ogal is to create a object
+//   // 3 parameters title, character, genre
+//   // book object should have property names that match the test cases (look at dot notation)
+//   // test cases call createTitle, buildMain
+//   // we need to use pageCount in this function
+//   let bookObj = {
+//     title,
+//     mainCharacter: character,
+//     pageCount: calculatePageCount(title),
+//     genre
+//   }
+//   return bookObj;
+// }
+
+let writeBook = (title, character, genre) => {
+  return {title, mainCharacter: character, pageCount: calculatePageCount(title), genre}
 }
 
-function writeBook(title, character, genre) {
-  // ogal is to create a object
-  // 3 parameters title, character, genre
-  // book object should have property names that match the test cases (look at dot notation)
-  // test cases call createTitle, buildMain
-  // we need to use pageCount in this function
-  let bookObj = {
-    title,
-    mainCharacter: character,
-    pageCount: calculatePageCount(title),
-    genre
-  }
-  return bookObj;
-}
+// function editBook(book) {
+//   // takes 1 parameter: writeBook - book
+//   // goal is to decrease page count to be 3/4 of original
+//   // use dot notation to access book objs pageCount property
+//   book.pageCount = book.pageCount * 0.75;
+// }
 
-function editBook(book) {
-  // takes 1 parameter: writeBook - book
-  // goal is to decrease page count to be 3/4 of original
-  book.pageCount = book.pageCount * 0.75;
-}
+let editBook = book => book.pageCount = book.pageCount * 0.75;
 
 module.exports = {
   createTitle,
