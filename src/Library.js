95c01c8bd53352009031ba906b620e8989b1efaa
Christine <cenguyen9@gmail.com>
2022-03-10 18:41:09 -0600

    created createLibrary function and passed initial tests

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,19 @@
-
+function createLibrary(libraryName) {
+  // goal is to create a library object that contains the librrary name
+  // 1 parameter - name of library (name dot notation)
+  // object should already contain shelves with different genres
+  return {
+    name: libraryName,
+    "shelves": {
+      "fantasy": [],
+      "fiction": [],
+      "nonFiction": []
+    }
+  }
+}
 
 module.exports = {
-  // createLibrary,
+  createLibrary,
   // addBook,
   // checkoutBook
 };
