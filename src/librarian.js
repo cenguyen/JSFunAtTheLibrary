2fe8328d89cc036bc615986baa8d0b68b9e4c3be
Christine <cenguyen9@gmail.com>
2022-03-11 17:13:17 -0600

    added findBook and calcLateDays functions, passed tests

--- src/librarian.js
+++ src/librarian.js
@@ -15,6 +15,32 @@ class Librarian {
       return `Hello, ${patronName}!`
     }
   }
+
+  // test case creates library and adds book to shelf
+  // calling findBook checks shelf (searchShelf?) but returns different string
+  findBook(bookTitle) {
+    let bookGenres = [];
+    let thisShelf = this.library.shelves;
+    for (let genre in thisShelf) {
+      bookGenres.push(genre);
+    }
+    for (var i = 0; i < bookGenres.length; i++) {
+      if (thisShelf[bookGenres[i]][0].title === bookTitle) {
+        thisShelf[bookGenres[i]].splice(i, 1);
+        return `Yes, we have ${bookTitle}`;
+      } else {
+        return `Sorry, we do not have ${bookTitle}`;
+      }
+    }
+  }
+
+  calculateLateFee(lateDays) {
+    // returns amount to be paid
+    // .25 per day, rounded UP whole integer
+    // 1 param - num of day late
+    return Math.ceil(lateDays * 0.25);
+  }
 }
+
 
 module.exports = Librarian;
