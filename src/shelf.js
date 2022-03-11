77edbbbb9e7e4152ca109e075e1aa1b3645d3f5b
Christine <cenguyen9@gmail.com>
2022-03-11 12:11:06 -0600

    added shelfBook function and passed tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,6 +1,16 @@
+function shelfBook(book, shelfName) {
+  // takes 2 parameters -object var and name of shelf it should be on
+  // goal is to push book obj to the START of array
+  // shelf is an array. will already exist in var test
+  // shlf cannot hold more than 3 books
+  if (shelfName.length < 3) {
+    shelfName.unshift(book);
+  }
+  return shelfName;
+}
 
 module.exports = {
-  // shelfBook,
+  shelfBook,
   // unshelfBook,
   // listTitles,
   // searchShelf
