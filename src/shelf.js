75b6ffc4571fc63c9876fe233137837e13ac1832
Christine <cenguyen9@gmail.com>
2022-03-11 18:21:08 -0600

    tried refactoring and practicing arrow functions for shelfBook and unshelfBook

--- src/shelf.js
+++ src/shelf.js
@@ -1,25 +1,41 @@
-function shelfBook(book, shelfName) {
-  // takes 2 parameters -object var and name of shelf it should be on
-  // goal is to push book obj to the START of array
-  // shelf is an array. will already exist in var test
-  // shlf cannot hold more than 3 books
-  if (shelfName.length < 3) {
+// function shelfBook(book, shelfName) {
+//   // takes 2 parameters -object var and name of shelf it should be on
+//   // goal is to push book obj to the START of array
+//   // shelf is an array. will already exist in var test
+//   // shlf cannot hold more than 3 books
+//   if (shelfName.length < 3) {
+//     shelfName.unshift(book);
+//   }
+//   return shelfName;
+// }
+
+let shelfBook = (book, shelfName) => {
+  if (shelfName.length < 3) { 
     shelfName.unshift(book);
-  }
+  } 
   return shelfName;
 }
 
-function unshelfBook(bookName, shelfName) {
-  // takes 2 parameters- nname of book and name of shelf
-  // needs to search through shelf array and look for title
-  // when match is found, splice
-  for (var i = 0; i < shelfName.length; i++) {
-    if (shelfName[i].title === bookName) {
-      shelfName.splice(i, 1);
+// function unshelfBook(bookName, shelfName) {
+//   // takes 2 parameters- nname of book and name of shelf
+//   // needs to search through shelf array and look for title
+//   // when match is found, splice
+//   for (var i = 0; i < shelfName.length; i++) {
+//     if (shelfName[i].title === bookName) {
+//       shelfName.splice(i, 1);
+//     }
+//   }
+//   return shelfName;
+// }
+
+let unshelfBook = (bookName, shelfName) => {
+  shelfName.forEach((book, index) => {
+    if (book.title === bookName) {
+      shelfName.splice(index, 1)
     }
-  }
+  })
   return shelfName;
-}
+};
 
 function listTitles(shelfName) {
   // takes 1 parameter - shelf name
